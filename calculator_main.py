8b045412f85a053321712cf8a8a35aff3dbedfec
hseunghyeon <hsh5278@naver.com>
2022-12-02 15:33:14 +0900

    계산기 UI 수정 / 개선하기

--- calculator_main.py
+++ calculator_main.py
@@ -46,20 +46,23 @@ class Main(QDialog):
         calculator_layout.addWidget(button_division, 1, 3)
         calculator_layout.addWidget(button_remainder, 0, 0)
 
-        ### =, clear, backspace 버튼 생성
+       ### =, C, CE, backspace 버튼 생성 (수정)
         button_equal = QPushButton("=")
-        button_clear = QPushButton("Clear")
-        button_backspace = QPushButton("Backspace")
+        button_clear = QPushButton("C")
+        button_ce = QPushButton("CE")
+        button_backspace = QPushButton("<-")
 
-        ### =, clear, backspace 버튼 클릭 시 시그널 설정
+        ### =, C, CE, backspace 버튼 클릭 시 시그널 설정 (수정)
         button_equal.clicked.connect(self.button_equal_clicked)
         button_clear.clicked.connect(self.button_clear_clicked)
+        button_ce.clicked.connect(self.button_backspace_clicked)
         button_backspace.clicked.connect(self.button_backspace_clicked)
 
-        ### =, clear, backspace 버튼을 layout_clear_equal 레이아웃에 추가
-        layout_clear_equal.addWidget(button_clear)
-        layout_clear_equal.addWidget(button_backspace)
-        layout_clear_equal.addWidget(button_equal)
+        ### =, C, CE, backspace 버튼 calculator_layout 레이아웃에 추가 (수정)
+        calculator_layout.addWidget(button_equal, 5, 3)
+        calculator_layout.addWidget(button_clear, 0, 2)
+        calculator_layout.addWidget(button_ce, 0, 1)
+        calculator_layout.addWidget(button_backspace, 0, 3)
 
         ### 숫자 버튼 생성하고, layout_number 레이아웃에 추가
         ### 각 숫자 버튼을 클릭했을 때, 숫자가 수식창에 입력 될 수 있도록 시그널 설정
@@ -67,27 +70,32 @@ class Main(QDialog):
         for number in range(0, 10):
             number_button_dict[number] = QPushButton(str(number))
             number_button_dict[number].clicked.connect(lambda state, num = number:
-                                                       self.number_button_clicked(num))
-            if number >0:
-                x,y = divmod(number-1, 3)
-                layout_number.addWidget(number_button_dict[number], x, y)
-            elif number==0:
-                layout_number.addWidget(number_button_dict[number], 3, 1)
+                                                       self.number_button_clicked(num))                                          
+      
+        calculator_layout.addWidget(number_button_dict[1], 2, 0)
+        calculator_layout.addWidget(number_button_dict[0], 5, 1)
+        calculator_layout.addWidget(number_button_dict[2], 2, 1)
+        calculator_layout.addWidget(number_button_dict[3], 2, 2)
+        calculator_layout.addWidget(number_button_dict[4], 3, 0)
+        calculator_layout.addWidget(number_button_dict[5], 3, 1)
+        calculator_layout.addWidget(number_button_dict[6], 3, 2)
+        calculator_layout.addWidget(number_button_dict[7], 4, 0)
+        calculator_layout.addWidget(number_button_dict[8], 4, 1)
+        calculator_layout.addWidget(number_button_dict[9], 4, 2)
 
-        ### 소숫점 버튼과 00 버튼을 입력하고 시그널 설정
+       ### 소숫점 버튼과 00 버튼을 입력하고 시그널 설정
         button_dot = QPushButton(".")
         button_dot.clicked.connect(lambda state, num = ".": self.number_button_clicked(num))
-        layout_number.addWidget(button_dot, 3, 2)
+        calculator_layout.addWidget(button_dot, 5, 2)
 
         button_double_zero = QPushButton("00")
         button_double_zero.clicked.connect(lambda state, num = "00": self.number_button_clicked(num))
-        layout_number.addWidget(button_double_zero, 3, 0)
+        calculator_layout.addWidget(button_double_zero, 5, 0)
+
 
         ### 각 레이아웃을 main_layout 레이아웃에 추가
-        main_layout.addLayout(layout_equation_solution)
-        main_layout.addLayout(layout_operation)
-        main_layout.addLayout(layout_clear_equal)
-        main_layout.addLayout(layout_number)
+        main_layout.addLayout(layout_display)
+        main_layout.addLayout(calculator_layout)
 
         self.setLayout(main_layout)
         self.show()
